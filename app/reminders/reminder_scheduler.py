d2931cc57e4cb70c9583c8a9e46e94366e1820ed
miki-reynolds <98548191+miki-reynolds@users.noreply.github.com>
2022-06-05 23:09:54 -0500

    ggc

--- app/reminders/reminder_scheduler.py
+++ app/reminders/reminder_scheduler.py
@@ -5,18 +5,17 @@ import os
 
 CREDENTIALS_JSON = {"web":{
   "client_id":"665751074662-5gieh3ii6km8p2pqli7m8e7tpimvl1e1.apps.googleusercontent.com",
-  "project_id":"bearpill-diary",
-  "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+  "project_id":"bearpill-diary","auth_uri":"https://accounts.google.com/o/oauth2/auth",
   "token_uri":"https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
   "client_secret":"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",
-  "redirect_uris":["https://adorable-semifreddo-f5be04.netlify.app/"]
-}}
+  "redirect_uris":["http://localhost:8080/","https://adorable-semifreddo-f5be04.netlify.app/"]}}
 
 # CLIENT_STR_VAR = os.environ.get('CLIENT_STR_VAR')
+with open("google-credentials.json", "w") as ggc:
+    json.dump(CREDENTIALS_JSON, ggc, indent=2)
 
-with open('google-credentials.json', 'w') as f:
-    CLIENT_SECRET_FILE = json.dump(CREDENTIALS_JSON, f, indent=2)
+CLIENT_SECRET_FILE = "google-credentials.json"
 
 API_NAME = 'calendar'
 API_VERSION = 'v3'
