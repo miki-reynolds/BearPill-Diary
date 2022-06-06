3077e68c1c2549b5aa2f6272b0b161d1e625b301
miki-reynolds <98548191+miki-reynolds@users.noreply.github.com>
2022-06-05 22:21:14 -0500

    gc

--- app/reminders/reminder_scheduler.py
+++ app/reminders/reminder_scheduler.py
@@ -1,12 +1,30 @@
 from app.reminders.google_api import Create_Service, convert_to_RFC_datetime
+import json
+import os
 
 
-CLIENT_SECRET_FILE = 'google-credentials.json'
+CLIENT_STR = '''
+{"web":{
+  "client_id":"665751074662-5gieh3ii6km8p2pqli7m8e7tpimvl1e1.apps.googleusercontent.com",
+  "project_id":"bearpill-diary",
+  "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+  "token_uri":"https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+  "client_secret":"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",
+  "redirect_uris":["https://adorable-semifreddo-f5be04.netlify.app/"]
+}}
+'''
+
+# CLIENT_STR_VAR = os.environ.get('CLIENT_STR_VAR')
+
+# CLIENT_SECRET_FILE = 'google-credentials.json'
+CLIENT_SECRET_FILE = json.loads(CLIENT_STR)
 API_NAME = 'calendar'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 # http://localhost:8080/
+
 
 def reminder_template(summary, description, attendee_name, attendee_email, start_date, end_date, freq, freq_interval, freq_byday):
     template = {
